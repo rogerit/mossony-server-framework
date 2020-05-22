220fd6504b4ca310d5ec96da262621522e830758
rogerit <775052803@qq.com>
2020-05-22 09:05:53 +0800

    https 跨域访问 cookies携带

--- src/main/java/com/mossony/framwork/filter/CORSFilter.java
+++ src/main/java/com/mossony/framwork/filter/CORSFilter.java
@@ -1,8 +1,10 @@
 package com.mossony.framwork.filter;
 
+import io.undertow.servlet.spec.HttpServletRequestImpl;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -15,8 +17,10 @@ public class CORSFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException, IOException {
+        //跨域fetch请求，*不能携带cookies，必须是真实origin
+        String origin = ((HttpServletRequest) servletRequest).getHeader("Origin");
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Origin", origin);
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "*");
