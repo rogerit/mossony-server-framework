5eca629e0ee2fbd42a28f7ecb3da27a6033e2d44
rogerit <775052803@qq.com>
2020-05-28 09:37:02 +0800

    文件上传

--- src/main/java/com/mossony/framwork/MossServer.java
+++ src/main/java/com/mossony/framwork/MossServer.java
@@ -5,6 +5,7 @@ import com.mossony.framwork.postman.PostManServlet;
 
 import javax.inject.Inject;
 import javax.servlet.DispatcherType;
+import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletException;
 
 import io.undertow.Handlers;
@@ -18,6 +19,7 @@ import io.undertow.server.handlers.resource.ResourceManager;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
 import io.undertow.servlet.api.FilterInfo;
+import io.undertow.servlet.api.ServletInfo;
 import io.undertow.servlet.util.ImmediateInstanceHandle;
 import lombok.extern.slf4j.Slf4j;
 
@@ -39,14 +41,17 @@ public class MossServer {
 
     public void startServer(int port) {
         try {
+            ServletInfo mossServlet = servlet("MossServlet", MossServlet.class, () -> new ImmediateInstanceHandle(this.mossServlet)).addMapping("/*");
+            mossServlet.setMultipartConfig(new MultipartConfigElement(System.getProperty("java.io.tmpdir")));
+            ServletInfo postmanServlet = servlet("postman", PostManServlet.class, () -> new ImmediateInstanceHandle(postManServlet)).addMapping("/postman");
             DeploymentInfo servletBuilder = deployment()
                     .setClassLoader(this.getClass().getClassLoader())
                     .setContextPath("/")
                     .setDeploymentName(this.getClass().getSimpleName())
                     .addFilter(new FilterInfo("CORS", CORSFilter.class))
                     .addFilterUrlMapping("CORS", "/*", DispatcherType.REQUEST)
-                    .addServlets(servlet("postman", PostManServlet.class, () -> new ImmediateInstanceHandle(postManServlet)).addMapping("/postman"))
-                    .addServlets(servlet("MossServlet", MossServlet.class, () -> new ImmediateInstanceHandle(mossServlet)).addMapping("/*"));
+                    .addServlets(postmanServlet)
+                    .addServlets(mossServlet);
             DeploymentManager manager = defaultContainer().addDeployment(servletBuilder);
             manager.deploy();
             HttpHandler servletHandler = manager.start();
